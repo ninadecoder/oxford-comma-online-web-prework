f8dd49eb1be9d1f671792620c98c2ee39b120fdd
Nina Kazzim <kazzim@icloud.com>
2019-06-02 19:54:16 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
+def oxford_comma(arr)
 
+  size = arr.size
+
+  if size == 1
+    return arr.join
+  elsif size == 2
+    return arr.join(" and ")
+  else size >= 3
+    last = arr.last #last element in array
+    arr.pop #takes out last
+    first = arr.join(", ") #adds ',' to all elements remaining
+    return "#{first}, and #{last}"
+  end
 end
