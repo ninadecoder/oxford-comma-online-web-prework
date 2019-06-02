6403935b35aaf07edf1453d79ec071922fb1276c
Nina Kazzim <kazzim@icloud.com>
2019-06-02 19:53:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,11 +5,11 @@ def oxford_comma(arr)
   if size == 1
     return arr.join
   elsif size == 2
-    puts arr.join(" and ")
+    return arr.join(" and ")
   else size >= 3
     last = arr.last #last element in array
     arr.pop #takes out last
     first = arr.join(", ") #adds ',' to all elements remaining
-    puts "#{first}, and #{last}"
+    return "#{first}, and #{last}"
   end
 end
